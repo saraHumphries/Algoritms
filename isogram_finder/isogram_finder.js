eb85b68321d880d722795ef9bdf7a7acf2a8cace
sara <smhumphries@hotmail.co.uk>
2021-06-16 15:45:54 +0100

    Isogram finder passes tests" "

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,25 @@
 const IsogramFinder = function (word) {
+    this.word = word.split('');
 
-}
+};
 
 IsogramFinder.prototype.isIsogram = function () {
+    const letterCounter = {};
+    for (letter of this.word) {
+        if (letterCounter[letter]) {
+            letterCounter[letter] += 1;
+        }
+        else {
+            letterCounter[letter] = 1;
+        };
+    };
+    for (value of Object.values(letterCounter)) {
+        if (value > 1) {
+            return false;
+        };
 
-}
+    };
+    return true;
+};
 
 module.exports = IsogramFinder;
