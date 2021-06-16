54d48cf218bd876c9beed63371b40bf7e6fe553f
sara <smhumphries@hotmail.co.uk>
2021-06-16 16:31:27 +0100

    anagrams passes first test

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,37 @@
 const AnagramFinder = function (word) {
+    this.word = word.split('');
 
-}
+};
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
+    
+    const wordCompare = {};
+    for (word of otherWords) {
+        const splitWord = word.split('');
+        wordCompare[word] = [];
+        for (letter of splitWord) {
+            if (this.word.includes(letter)) {
+                wordCompare[word].push(true);
+            }
+            else {
+                wordCompare[word].push(false);
+            };
+        };
+    };
+    
+    const anagrams = [];
+    for (word of otherWords) {
 
-}
+        const result = wordCompare[word].every((letterBool) => {
+            return letterBool;  
+        });
+        if (result) {
+            anagrams.push(word);
+        };        
+    };
+    return anagrams;
+
+
+};
 
 module.exports = AnagramFinder;
