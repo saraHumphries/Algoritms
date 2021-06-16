b0cf62fb2892186716eba07b1e7e197ee2003aa5
sara <smhumphries@hotmail.co.uk>
2021-06-16 14:55:03 +0100

    upperCaser passes tests

--- upper_caser/upper_caser.js
+++ upper_caser/upper_caser.js
@@ -1,8 +1,14 @@
 const UpperCaser = function (words) {
-}
+    this.words = words;
+};
 
 UpperCaser.prototype.toUpperCase = function () {
+    const upperCaseWords = [];
+    for (word of this.words) {
+        upperCaseWords.push(word.toUpperCase()); 
+    };
+    return upperCaseWords;
 
-}
+};
 
 module.exports = UpperCaser;
