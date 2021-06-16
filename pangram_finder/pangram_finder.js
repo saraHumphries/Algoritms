00822790ef6cb395929972a69c7a78b183ef858e
sara <smhumphries@hotmail.co.uk>
2021-06-16 15:35:05 +0100

    given up on Panagram for now

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,25 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
-}
+  this.phrase = phrase.split('');
+};
 
 PangramFinder.prototype.isPangram = function () {
+  
+  const tracker = {};
+  for (letter of this.alphabet) {
+    for (character of this.phrase) {
+      if (letter === character) {
+        tracker[letter] = true;
+      }
+      else {
+        tracker[letter] = false;
+      };
+    };
+  };
+  console.log(tracker);
+};
 
-}
+
 
 module.exports = PangramFinder;
+
