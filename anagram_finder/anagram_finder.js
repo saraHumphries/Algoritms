affeb85bf37dcce025a84ad7a0b8288f10726874
sara <smhumphries@hotmail.co.uk>
2021-06-17 09:37:52 +0100

    refactors anagram

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,42 +1,70 @@
-const AnagramFinder = function (word) {
-    this.word = word.toLowerCase().split('');
+// const AnagramFinder = function (word) {
+//     this.word = word.toLowerCase().split('');
 
-};
+// };
 
-AnagramFinder.prototype.findAnagrams = function (otherWords) {
+// AnagramFinder.prototype.findAnagrams = function (otherWords) {
+   
+//     const lowerCaseOtherWords = otherWords.map(word => word.toLowerCase()); 
 
+//     const wordCompare = {};
+//     for (const word of lowerCaseOtherWords) {
+//         const splitWord = word.split('');
+//         wordCompare[word] = [];
+//         for (letter of splitWord) {
+//             if (this.word.includes(letter)) {
+//                 wordCompare[word].push(true);
+//             }
+//             else {
+//                 wordCompare[word].push(false);
+//             };
+//         };
+//     };
     
-    const lowerCaseOtherWords = otherWords.map(word => word.toLowerCase()); 
+//     const anagrams = [];
+//     for (const word of otherWords) {
 
-    const wordCompare = {};
-    for (const word of lowerCaseOtherWords) {
-        const splitWord = word.split('');
-        wordCompare[word] = [];
-        for (letter of splitWord) {
-            if (this.word.includes(letter)) {
-                wordCompare[word].push(true);
-            }
-            else {
-                wordCompare[word].push(false);
-            };
-        };
-    };
-    
-    const anagrams = [];
-    for (const word of otherWords) {
-
-        const result = wordCompare[word.toLowerCase()].every((letterBool) => {
-            return letterBool;  
-        });
-        if (result && this.word.length === word.length && this.word.join('') !== word) {
-            anagrams.push(word);
-        };       
+//         const result = wordCompare[word.toLowerCase()].every((letterBool) => {
+//             return letterBool;  
+//         });
+//         if (result && this.word.length === word.length && this.word.join('') !== word) {
+//             anagrams.push(word);
+//         };       
          
-    };
+//     };
     
-    return anagrams;
+//     return anagrams;
 
+
+// };
+
+
+
+// prepare both into same format by spliting into a list, then ordering, then re-forming into a string.
+// check if not the same original word (lowercase) and that are the same after the prep is done
+
+const AnagramFinder = function (word) {
+    this.word = word;
 
 };
+
+AnagramFinder.prototype.findAnagrams = function(otherWords) {
+    return otherWords.filter((otherWord) => {
+        return this.isNotTheSameWord(otherWord) && this.isAnagram(otherWord);
+    });
+};
+
+AnagramFinder.prototype.isNotTheSameWord = function (otherWord) {
+    return this.word.toLowerCase() !== otherWord.toLowerCase();
+};
+
+AnagramFinder.prototype.prepareWord = function(word) {
+    return word.toLowerCase().split('').sort().join('');
+};
+
+AnagramFinder.prototype.isAnagram = function(otherWord) {
+    return this.prepareWord(this.word) === this.prepareWord(otherWord);  
+};
+
 
 module.exports = AnagramFinder;
